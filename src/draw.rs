ce858bbf412927602cc10b6d9759e3eff781b816
rhysd <lin90162@yahoo.co.jp>
2019-01-02 22:25:15 +0900

    reduce times for setting color

--- src/draw.rs
+++ src/draw.rs
@@ -1,19 +1,23 @@
 extern crate termcolor;
 
-use self::termcolor::WriteColor;
+use self::termcolor::{ColorSpec, WriteColor};
 use crate::board::Board;
 use crate::error::Result;
 
 struct TermDrawer<W: WriteColor> {
     stdout: W,
     show_altitude: bool,
+    current: ColorSpec,
 }
 
 impl<W: WriteColor> TermDrawer<W> {
     fn draw(&mut self, board: &Board) -> Result<()> {
         for row in board.rows() {
             for cell in row.cols() {
-                self.stdout.set_color(&cell.color)?;
+                if self.current != cell.color {
+                    self.stdout.set_color(&cell.color)?;
+                    self.current = cell.color.clone();
+                }
                 if self.show_altitude {
                     write!(&mut self.stdout, "{:02}", cell.altitude)?;
                 } else {
@@ -30,6 +34,7 @@ pub fn draw_term(board: &Board, show_altitude: bool) -> Result<()> {
     let mut drawer = TermDrawer {
         stdout: termcolor::StandardStream::stdout(termcolor::ColorChoice::Always),
         show_altitude,
+        current: ColorSpec::default(),
     };
     drawer.draw(board)
 }
