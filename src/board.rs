1aa02538962ccb85f52f7ae3e2780b6905b44d31
rhysd <lin90162@yahoo.co.jp>
2019-01-13 22:59:27 +0900

    include legends list in serialized JSON output

--- src/board.rs
+++ src/board.rs
@@ -2,6 +2,7 @@ extern crate serde;
 extern crate serde_json;
 
 use crate::land::Land;
+use std::collections::HashMap;
 use std::ops::{Index, IndexMut};
 
 //       width
@@ -31,15 +32,15 @@ impl Pos {
     }
 }
 
-#[derive(Debug, PartialEq, Serialize)]
+#[derive(Debug, PartialEq)]
 pub struct Board<'a> {
     pub width: usize,
     pub height: usize,
-    rows: Vec<Vec<Land<'a>>>,
+    cells: Vec<Vec<Land<'a>>>,
 }
 
 pub struct Iter<'a> {
-    rows: &'a [Vec<Land<'a>>],
+    cells: &'a [Vec<Land<'a>>],
     x: usize,
     y: usize,
 }
@@ -49,14 +50,14 @@ impl<'a> Iterator for Iter<'a> {
 
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.rows.len(), None)
+        (self.cells.len(), None)
     }
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.y >= self.rows.len() {
+        if self.y >= self.cells.len() {
             return None;
         }
-        let row = &self.rows[self.y];
+        let row = &self.cells[self.y];
         if self.x < row.len() {
             self.x += 1;
             Some(&row[self.x - 1])
@@ -73,16 +74,16 @@ impl<'a> Board<'a> {
     where
         F: FnMut(usize, usize) -> Land<'a>,
     {
-        let mut rows = Vec::with_capacity(height);
+        let mut cells = Vec::with_capacity(height);
         for y in 0..height {
             let mut cols = Vec::with_capacity(width);
             for x in 0..width {
                 cols.push(builder(x, y));
             }
-            rows.push(cols);
+            cells.push(cols);
         }
         Board {
-            rows,
+            cells,
             width,
             height,
         }
@@ -100,23 +101,23 @@ impl<'a> Board<'a> {
 
     #[inline]
     pub fn at(&self, x: usize, y: usize) -> &Land {
-        &self.rows[y][x]
+        &self.cells[y][x]
     }
 
     #[inline]
     pub fn rows(&self) -> std::slice::Iter<Vec<Land<'a>>> {
-        self.rows.iter()
+        self.cells.iter()
     }
 
     #[inline]
     pub fn at_mut<'b>(&mut self, p: &'b Pos) -> &'a mut Land {
-        &mut self.rows[p.y][p.x]
+        &mut self.cells[p.y][p.x]
     }
 
     #[inline]
     pub fn iter(&self) -> Iter {
         Iter {
-            rows: &self.rows,
+            cells: &self.cells,
             x: 0,
             y: 0,
         }
@@ -128,14 +129,33 @@ impl<'a> Index<Pos> for Board<'a> {
 
     #[inline]
     fn index(&self, p: Pos) -> &Land<'a> {
-        &self.rows[p.y][p.x]
+        &self.cells[p.y][p.x]
     }
 }
 
 impl<'a> IndexMut<Pos> for Board<'a> {
     #[inline]
     fn index_mut(&mut self, p: Pos) -> &mut Land<'a> {
-        &mut self.rows[p.y][p.x]
+        &mut self.cells[p.y][p.x]
+    }
+}
+
+impl<'a> serde::Serialize for Board<'a> {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        use serde::ser::SerializeMap;
+        let mut map = serializer.serialize_map(Some(4))?;
+        map.serialize_entry("width", &self.width)?;
+        map.serialize_entry("height", &self.height)?;
+        map.serialize_entry("cells", &self.cells)?;
+
+        // Do not derive Serialize trait since legends should be contained in serialized JSON output
+        let legends = self
+            .iter()
+            .map(|cell| (cell.kind, cell.kind.legend()))
+            .collect::<HashMap<_, _>>();
+        map.serialize_entry("legends", &legends)?;
+
+        map.end()
     }
 }
 
